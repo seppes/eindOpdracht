61a7e1b0a0c644eac01f50012a42d73bcb2b5eea
seppe <r0787714@thomasmore.be>
2020-10-12 23:23:40 +0200

    landen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import styled from "@emotion/styled";
 
-function Kamers (props){
+function Kamers(props) {
     const {kamer} = props;
     return <div className="productLine">
         <div>{kamer.soort}</div>
-        <div>{kamer.aantalersonen}</div>
+        <div>{kamer.aantalPersonen}</div>
         <div>{kamer.prijs}&euro;</div>
+    </div>;
+}
+
+function Landen(props) {
+    const {landen} = props;
+    return <div className="productLine">
+        <div>{landen.land}</div>
+        <div>{landen.aantalHotels}</div>
     </div>;
 }
 
@@ -22,23 +31,43 @@ function Footer() {
     </div>;
 }
 
-const KAMER_DETAILS = [
-    {soort : "eenpersoonskamer", aantalersonen : 1, prijs : 50},
-    {soort : "tweepersoonskamer", aantalersonen : 2, prijs : 80},
-    {soort : "vierpersoonskamer", aantalersonen : 4, prijs : 150},
-    {soort : "vijfpersoonskamer", aantalersonen : 5, prijs : 200},
-    {soort : "tienpersoonskamer", aantalersonen : 10, prijs : 300},
-];
+const StyleH1 = styled.h1`
+    border-bottom: black;
+    font-weight: lighter;
+`;
 
+const StyleKamers = styled.div`
+    display: inline-block;
+    padding-bottomt: 40px;
+`;
+
+
+const KAMER_DETAILS = [
+    {soort: "eenpersoonskamer", aantalPersonen: 1, prijs: 50},
+    {soort: "tweepersoonskamer", aantalPersonen: 2, prijs: 80},
+    {soort: "vierpersoonskamer", aantalPersonen: 4, prijs: 150},
+    {soort: "vijfpersoonskamer", aantalPersonen: 5, prijs: 200},
+    {soort: "tienpersoonskamer", aantalPersonen: 10, prijs: 300}
+];
+const LANDEN = [
+    {land: "Belgie", aantalHotels: 6},
+    {land: "Nederland", aantalHotels: 3},
+    {land: "Frankrijk", aantalHotels: 4},
+];
 function App() {
     return (
         <div id="root">
             <Navigatie/>
             <div>
-                <h1>Hotel</h1>
-                {KAMER_DETAILS.map((p) =>
-                    <Kamers key ={p.soort} kamer={p}/>)}
-                    </div>
+                <StyleH1>Hotel</StyleH1>
+                {KAMER_DETAILS.map((k) =>
+                    <Kamers key={k.soort} kamer={k}/>)}
+            </div>
+            <div>
+                <StyleH1>Landen</StyleH1>
+                {LANDEN.map((l) =>
+                    <Landen key={l.land} landen={l}/>)}
+            </div>
             <Footer/>
         </div>
     );
