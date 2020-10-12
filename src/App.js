03e6d43701d8e560df0f6cc9d52696dee57ff78e
seppe <r0787714@thomasmore.be>
2020-10-12 23:39:22 +0200

    componenten gemaakt in styles... daarom heb ik er geen 5
    + opkuis code

--- src/App.js
+++ src/App.js
@@ -19,45 +19,66 @@ function Landen(props) {
     </div>;
 }
 
-function Navigatie() {
-    return <div>
-        <h2>ez bookings copyright 2020</h2>
-    </div>;
-}
-
-function Footer() {
-    return <div>
-        <a href="/">ez bookings</a>
-    </div>;
-}
-
 const StyleH1 = styled.h1`
     border-bottom: black;
     font-weight: lighter;
+    padding-top: 80px;
 `;
 
-const StyleKamers = styled.div`
-    display: inline-block;
-    padding-bottomt: 40px;
+const StyleNav = styled.div`
+  height: 60px;
+  padding-top: 30px;
+  background-color: cadetblue;
+  color: white;
+  font-weight: bold;
+  text-decoration: none;
+  text-align: center;
+  padding-left: 30px;
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
 `;
 
+const StyleFooter = styled.div`
+  text-align: center;
+  background-color: lightblue;
+  color: white;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 50px;
+  width: 100%;
+  margin-block-start: 0em;
+  margin-block-end: 0em;
+`;
+
+const StyleNavLink = styled.a`
+  color: white;
+  font-size: larger;
+  font-weight: bold;
+  text-decoration: none;
+  text-align: center;
+`;
 
 const KAMER_DETAILS = [
-    {soort: "eenpersoonskamer", aantalPersonen: 1, prijs: 50},
-    {soort: "tweepersoonskamer", aantalPersonen: 2, prijs: 80},
-    {soort: "vierpersoonskamer", aantalPersonen: 4, prijs: 150},
-    {soort: "vijfpersoonskamer", aantalPersonen: 5, prijs: 200},
-    {soort: "tienpersoonskamer", aantalPersonen: 10, prijs: 300}
+    {soort: "eenpersoons kamer", aantalPersonen: 1, prijs: 50},
+    {soort: "tweepersoons kamer", aantalPersonen: 2, prijs: 80},
+    {soort: "vierpersoons kamer", aantalPersonen: 4, prijs: 150},
+    {soort: "vijfpersoons kamer", aantalPersonen: 5, prijs: 200},
+    {soort: "tienpersoons kamer", aantalPersonen: 10, prijs: 300}
 ];
+
 const LANDEN = [
     {land: "Belgie", aantalHotels: 6},
     {land: "Nederland", aantalHotels: 3},
     {land: "Frankrijk", aantalHotels: 4},
 ];
+
 function App() {
     return (
         <div id="root">
-            <Navigatie/>
+            <StyleNav><StyleNavLink href="/">ez bookings</StyleNavLink></StyleNav>
             <div>
                 <StyleH1>Hotel</StyleH1>
                 {KAMER_DETAILS.map((k) =>
@@ -68,7 +89,7 @@ function App() {
                 {LANDEN.map((l) =>
                     <Landen key={l.land} landen={l}/>)}
             </div>
-            <Footer/>
+            <StyleFooter><p>ez bookings copyright 2020</p></StyleFooter>
         </div>
     );
 }
