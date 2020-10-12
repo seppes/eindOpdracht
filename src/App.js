0aeb5322b422b688c569b493132b60fd7dc0711b
seppe <r0787714@thomasmore.be>
2020-10-12 23:08:38 +0200

    navbar, footer en array van kamers

--- src/App.js
+++ src/App.js
@@ -2,22 +2,44 @@ import React from 'react';
 import './App.css';
 
 function Kamers (props){
-    const {kamers} = props;
-    return <div>{kamers}</div>
+    const {kamer} = props;
+    return <div className="productLine">
+        <div>{kamer.soort}</div>
+        <div>{kamer.aantalersonen}</div>
+        <div>{kamer.prijs}&euro;</div>
+    </div>;
 }
 
+function Navigatie() {
+    return <div>
+        <h2>ez bookings copyright 2020</h2>
+    </div>;
+}
+
+function Footer() {
+    return <div>
+        <a href="/">ez bookings</a>
+    </div>;
+}
+
+const KAMER_DETAILS = [
+    {soort : "eenpersoonskamer", aantalersonen : 1, prijs : 50},
+    {soort : "tweepersoonskamer", aantalersonen : 2, prijs : 80},
+    {soort : "vierpersoonskamer", aantalersonen : 4, prijs : 150},
+    {soort : "vijfpersoonskamer", aantalersonen : 5, prijs : 200},
+    {soort : "tienpersoonskamer", aantalersonen : 10, prijs : 300},
+];
 
 function App() {
     return (
         <div id="root">
+            <Navigatie/>
             <div>
                 <h1>Hotel</h1>
-                <Kamers kamers= "eenpersoonskamer"/>
-                <Kamers kamers= "tweepersoonskamer"/>
-                <Kamers kamers= "vierpersoonskamer"/>
-                <Kamers kamers= "vijfpersoonskamer"/>
-                <Kamers kamers= "tienpersoonskamer"/>
-            </div>
+                {KAMER_DETAILS.map((p) =>
+                    <Kamers key ={p.soort} kamer={p}/>)}
+                    </div>
+            <Footer/>
         </div>
     );
 }
