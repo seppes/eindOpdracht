065674de647f3fcde232a323dcafa4d22304cd97
seppe <r0787714@thomasmore.be>
2020-10-12 23:44:07 +0200

    Updates

---
+++ precache-manifest.ca5e21594c2cbf42ffbe1977bbef4d92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef6c6dc8fcfbd9a159345ea53105342",
+    "url": "/eindOpdracht/index.html"
+  },
+  {
+    "revision": "1ee46e45efb4dd6269ee",
+    "url": "/eindOpdracht/static/css/main.195d5c2b.chunk.css"
+  },
+  {
+    "revision": "c96f22a6eb34f9bb1572",
+    "url": "/eindOpdracht/static/js/2.3d14df54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eindOpdracht/static/js/2.3d14df54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee46e45efb4dd6269ee",
+    "url": "/eindOpdracht/static/js/main.42cd34b3.chunk.js"
+  },
+  {
+    "revision": "e8c01d91418418d0de97",
+    "url": "/eindOpdracht/static/js/runtime-main.b3d09753.js"
+  }
+]);
